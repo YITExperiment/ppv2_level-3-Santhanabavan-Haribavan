42155fe8c90e2b30e7cfce49b020680721b7143c
Santhanabavan-Haribavan <134407142+Santhanabavan-Haribavan@users.noreply.github.com>
2023-05-23 20:56:50 +0530

    Add files via upload

---
+++ My frist program output.py
@@ -0,0 +1,7 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: C:/Users/DELL/Desktop/My frist program.py ==============
+Hello,world!
+What is your name?
+Hello 
